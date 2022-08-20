b1299c1e37395dd7d904a72fc296604c0cd684c6
developersung13 <developersung13@gmail.com>
2022-08-20 22:47:55 +0900

    [fix]: (log) 블록 추가 (#9)

---
+++ src/data/block_4_Collection.js
@@ -0,0 +1,70 @@
+export const block_4_Collection = {
+  name: "block_4",
+  title: "log",
+  areas: [
+    {
+      skills: [
+        {
+          name: "git init",
+          description: {
+            text: "Learn what accessibility is and why it is important.",
+            links: [
+              [
+                "Google Devs - Accessibility Fundamentals",
+                "https://developers.google.com/web/fundamentals/accessibility/",
+              ],
+              [
+                "A11ycasts with Rob Dodson",
+                "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g",
+              ],
+              [
+                "Udacity - Chromevox Lite",
+                "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/",
+              ],
+              [
+                "Dev.to - Why Accessibility Matters",
+                "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl",
+              ],
+              [
+                "Abilitynet - Why Accessibility Matters",
+                "https://www.abilitynet.org.uk/why-accessibility-matters",
+              ],
+              [
+                "Udacity - Web Accessibility",
+                "https://www.udacity.com/course/web-accessibility--ud891",
+              ],
+              [
+                "Youtube - Headings, Landmarks, and Tabs",
+                "https://www.youtube.com/watch?v=HE2R86EZPMA",
+              ],
+            ],
+          },
+        },
+        {
+          name: "git clone",
+          description: {
+            text: "Learn about assistive technology such as screen readers that reads sections of the page aloud based on the current focus.",
+            links: [
+              [
+                "The A11Y Project - Screen Reader Myths",
+                "https://a11yproject.com/posts/people-who-use-screen-readers-dont-use-javascript/",
+              ],
+              [
+                "The A11Y Project - NVDA Screen Reader",
+                "https://a11yproject.com/posts/getting-started-with-nvda/",
+              ],
+              [
+                "The A11Y Project - OS X Voiceover",
+                "https://a11yproject.com/posts/getting-started-with-voiceover/",
+              ],
+              [
+                "Webaim - Screen Reader Survey",
+                "https://webaim.org/projects/screenreadersurvey7/",
+              ],
+            ],
+          },
+        },
+      ],
+    },
+  ],
+};
