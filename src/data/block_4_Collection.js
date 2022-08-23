391b138d89c4d54d3a991364323066919d4d142f
chanmyeong <chanmyeongwoo@gmail.com>
2022-08-23 15:58:01 +0900

    [fix]: (4. Log) 블록 내부의 유닛 데이터 수정 (#28) (#29)
    * [fix]: git log 유닛의 설명 수정 (#28)
    
    * [fix]: git log url 주소 수정 (#28)

--- src/data/block_4_Collection.js
+++ src/data/block_4_Collection.js
@@ -1,41 +1,18 @@
 export const block_4_Collection = {
   name: "block_4",
-  title: "log",
+  title: "Log",
   areas: [
     {
       skills: [
         {
           name: "git log",
           description: {
-            text: "Learn what accessibility is and why it is important.",
+            text: "git log는 commit 히스토리를 시간의 역순으로 보여줄 때 사용하는 Git 명령어 입니다.",
             links: [
+              ["Git scm - log", "https://git-scm.com/docs/git-log"],
               [
-                "Google Devs - Accessibility Fundamentals",
-                "https://developers.google.com/web/fundamentals/accessibility/",
-              ],
-              [
-                "A11ycasts with Rob Dodson",
-                "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g",
-              ],
-              [
-                "Udacity - Chromevox Lite",
-                "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/",
-              ],
-              [
-                "Dev.to - Why Accessibility Matters",
-                "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl",
-              ],
-              [
-                "Abilitynet - Why Accessibility Matters",
-                "https://www.abilitynet.org.uk/why-accessibility-matters",
-              ],
-              [
-                "Udacity - Web Accessibility",
-                "https://www.udacity.com/course/web-accessibility--ud891",
-              ],
-              [
-                "Youtube - Headings, Landmarks, and Tabs",
-                "https://www.youtube.com/watch?v=HE2R86EZPMA",
+                "Git tutorial - log",
+                "https://backlog.com/git-tutorial/kr/reference/basic.html#sec6",
               ],
             ],
           },
