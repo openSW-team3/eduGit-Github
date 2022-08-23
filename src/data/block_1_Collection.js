4e316f563d50db4549bf5455aef37ae05456c5d6
Jusung_Park <juke2391@gmail.com>
2022-08-23 15:37:04 +0900

    [fix]: git clone 유닛의 url 주소 수정(#23)

--- src/data/block_1_Collection.js
+++ src/data/block_1_Collection.js
@@ -23,21 +23,10 @@ export const block_1_Collection = {
           description: {
             text: "git clone는  기존 사용되던 저장소를 복제하여 가져올때 사용하는 Git 명령어 입니다.",
             links: [
+              ["Git scm - clone", "https://git-scm.com/docs/git-clone"],
               [
-                "The A11Y Project - Screen Reader Myths",
-                "https://a11yproject.com/posts/people-who-use-screen-readers-dont-use-javascript/",
-              ],
-              [
-                "The A11Y Project - NVDA Screen Reader",
-                "https://a11yproject.com/posts/getting-started-with-nvda/",
-              ],
-              [
-                "The A11Y Project - OS X Voiceover",
-                "https://a11yproject.com/posts/getting-started-with-voiceover/",
-              ],
-              [
-                "Webaim - Screen Reader Survey",
-                "https://webaim.org/projects/screenreadersurvey7/",
+                "Git tutorial - clone",
+                "https://backlog.com/git-tutorial/kr/intro/intro3_2.html",
               ],
             ],
           },
