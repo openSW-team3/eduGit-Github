184458bf659170355103392834ba8fad26b655c9
chanmyeong <chanmyeongwoo@gmail.com>
2022-08-23 15:41:15 +0900

    [fix]: (3. Branch와 Merge) 블록 내부의 유닛 데이터 수정 (#25) (#26)
    * [fix]: git branch 유닛의 설명 수정 (#25)
    
    * [fix]: git branch url 주소 수정 (#25)
    
    * [fix]: git merge 유닛의 설명 수정 (#25)
    
    * [fix]: git merge url 주소 수정 (#25)

--- src/data/block_3_Collection.js
+++ src/data/block_3_Collection.js
@@ -7,35 +7,12 @@ export const block_3_Collection = {
         {
           name: "git branch",
           description: {
-            text: "Learn what accessibility is and why it is important.",
+            text: "git branch는 변경 사항의 스냅샷에 대한 포인터로 사용하는 Git 명령어 입니다.",
             links: [
+              ["Git scm - branch", "https://git-scm.com/docs/git-branch"],
               [
-                "Google Devs - Accessibility Fundamentals",
-                "https://developers.google.com/web/fundamentals/accessibility/",
-              ],
-              [
-                "A11ycasts with Rob Dodson",
-                "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g",
-              ],
-              [
-                "Udacity - Chromevox Lite",
-                "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/",
-              ],
-              [
-                "Dev.to - Why Accessibility Matters",
-                "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl",
-              ],
-              [
-                "Abilitynet - Why Accessibility Matters",
-                "https://www.abilitynet.org.uk/why-accessibility-matters",
-              ],
-              [
-                "Udacity - Web Accessibility",
-                "https://www.udacity.com/course/web-accessibility--ud891",
-              ],
-              [
-                "Youtube - Headings, Landmarks, and Tabs",
-                "https://www.youtube.com/watch?v=HE2R86EZPMA",
+                "Git tutorial - branch",
+                "https://backlog.com/git-tutorial/kr/reference/branch.html",
               ],
             ],
           },
@@ -43,23 +20,12 @@ export const block_3_Collection = {
         {
           name: "git merge",
           description: {
-            text: "Learn about assistive technology such as screen readers that reads sections of the page aloud based on the current focus.",
+            text: "git merge는 다른 branch를 현재 checkout된 branch와 합치고 merge 된 결과를 가리키도록 옮길 때 사용하는 Git 명령어 입니다.",
             links: [
+              ["Git scm - merge", "https://git-scm.com/docs/git-merge"],
               [
-                "The A11Y Project - Screen Reader Myths",
-                "https://a11yproject.com/posts/people-who-use-screen-readers-dont-use-javascript/",
-              ],
-              [
-                "The A11Y Project - NVDA Screen Reader",
-                "https://a11yproject.com/posts/getting-started-with-nvda/",
-              ],
-              [
-                "The A11Y Project - OS X Voiceover",
-                "https://a11yproject.com/posts/getting-started-with-voiceover/",
-              ],
-              [
-                "Webaim - Screen Reader Survey",
-                "https://webaim.org/projects/screenreadersurvey7/",
+                "Git tutorial - merge",
+                "https://backlog.com/git-tutorial/kr/reference/branch.html#sec6",
               ],
             ],
           },
