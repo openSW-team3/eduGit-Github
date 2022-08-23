1de470e09348cb7e8fd476a69e5984776f09576d
Jusung_Park <juke2391@gmail.com>
2022-08-23 16:38:54 +0900

    [fix]: git commit 유닛의 설명 내용 수정 (#30)

--- src/data/block_2_Collection.js
+++ src/data/block_2_Collection.js
@@ -47,7 +47,7 @@ export const block_2_Collection = {
         {
           name: "git commit",
           description: {
-            text: "Learn about assistive technology such as screen readers that reads sections of the page aloud based on the current focus.",
+            text: "git commit는 저장소에 하나의 스냅샷으로 기록할 떄 사용하는 Git 명령어 입니다.",
             links: [
               [
                 "The A11Y Project - Screen Reader Myths",
