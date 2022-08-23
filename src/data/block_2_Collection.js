a3bfdd6a9403c00f76dbd1d4c68c937818b4e942
Jusung_Park <juke2391@gmail.com>
2022-08-23 16:26:21 +0900

    [fix]: git status 유닛의 설명 내용 수정 (#30)

--- src/data/block_2_Collection.js
+++ src/data/block_2_Collection.js
@@ -7,7 +7,7 @@ export const block_2_Collection = {
         {
           name: "git add",
           description: {
-            text: "Working Direcotry 에서 파일/폴더를 Staging Area에 등록하여 git으로 관리를 시작 하기위해 사용하는 Git 명령어 입니다.",
+            text: "git add는 Working Direcotry 에서 파일/폴더를 Staging Area에 등록하여 git으로 관리를 시작 하기위해 사용하는 Git 명령어 입니다.",
             links: [
               ["Git scm - add", "https://git-scm.com/docs/git-add"],
               [
@@ -21,23 +21,12 @@ export const block_2_Collection = {
         {
           name: "git status",
           description: {
-            text: "Learn about assistive technology such as screen readers that reads sections of the page aloud based on the current focus.",
+            text: "git status는 git의 현재 상태를 확인 할 떄 사용하는 Git 명령어 입니다.",
             links: [
+              ["Git scm - status", "https://git-scm.com/docs/git-status"],
               [
-                "The A11Y Project - Screen Reader Myths",
-                "https://a11yproject.com/posts/people-who-use-screen-readers-dont-use-javascript/",
-              ],
-              [
-                "The A11Y Project - NVDA Screen Reader",
-                "https://a11yproject.com/posts/getting-started-with-nvda/",
-              ],
-              [
-                "The A11Y Project - OS X Voiceover",
-                "https://a11yproject.com/posts/getting-started-with-voiceover/",
-              ],
-              [
-                "Webaim - Screen Reader Survey",
-                "https://webaim.org/projects/screenreadersurvey7/",
+                "Git tutorial - status",
+                "https://backlog.com/git-tutorial/kr/reference/basic.html#sec4",
               ],
             ],
           },
