89abd3c60f45ac3890d71b327a2a8b5f7a8cd68d
developersung13 <developersung13@gmail.com>
2022-08-23 21:12:26 +0900

    [fix]: git stash URL 주소 수정 (#33)

--- src/data/block_6_Collection.js
+++ src/data/block_6_Collection.js
@@ -9,33 +9,10 @@ export const block_6_Collection = {
           description: {
             text: "git stash는 변경사항을 임시로 저장할 수 있도록 도와주는 Git 명령어입니다.",
             links: [
+              ["Git scm - stash", "https://git-scm.com/docs/git-stash"],
               [
-                "Google Devs - Accessibility Fundamentals",
-                "https://developers.google.com/web/fundamentals/accessibility/",
-              ],
-              [
-                "A11ycasts with Rob Dodson",
-                "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g",
-              ],
-              [
-                "Udacity - Chromevox Lite",
-                "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/",
-              ],
-              [
-                "Dev.to - Why Accessibility Matters",
-                "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl",
-              ],
-              [
-                "Abilitynet - Why Accessibility Matters",
-                "https://www.abilitynet.org.uk/why-accessibility-matters",
-              ],
-              [
-                "Udacity - Web Accessibility",
-                "https://www.udacity.com/course/web-accessibility--ud891",
-              ],
-              [
-                "Youtube - Headings, Landmarks, and Tabs",
-                "https://www.youtube.com/watch?v=HE2R86EZPMA",
+                "Git tutorial - stash",
+                "https://backlog.com/git-tutorial/kr/reference/stash.html",
               ],
             ],
           },
