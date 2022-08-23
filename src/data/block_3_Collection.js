b12a36336cb46328a2721b20f23d93dc57c53351
chanmyeong <64055771+chanmyeong@users.noreply.github.com>
2022-08-23 15:27:41 +0900

    [fix]: git merge url 주소 수정 (#25)

--- src/data/block_3_Collection.js
+++ src/data/block_3_Collection.js
@@ -22,21 +22,10 @@ export const block_3_Collection = {
           description: {
             text: "git merge는 다른 branch를 현재 checkout된 branch와 합치고 merge 된 결과를 가리키도록 옮길 때 사용하는 Git 명령어 입니다.",
             links: [
+              ["Git scm - merge", "https://git-scm.com/docs/git-merge"],
               [
-                "The A11Y Project - Screen Reader Myths",
-                "https://a11yproject.com/posts/people-who-use-screen-readers-dont-use-javascript/",
-              ],
-              [
-                "The A11Y Project - NVDA Screen Reader",
-                "https://a11yproject.com/posts/getting-started-with-nvda/",
-              ],
-              [
-                "The A11Y Project - OS X Voiceover",
-                "https://a11yproject.com/posts/getting-started-with-voiceover/",
-              ],
-              [
-                "Webaim - Screen Reader Survey",
-                "https://webaim.org/projects/screenreadersurvey7/",
+                "Git tutorial - merge",
+                "https://backlog.com/git-tutorial/kr/reference/branch.html#sec6",
               ],
             ],
           },
