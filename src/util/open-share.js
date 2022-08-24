b762c2530510b97fa8a516ce382c0ab24fef22a4
developersung13 <developersung13@gmail.com>
2022-08-24 15:47:14 +0900

    [fix]: 공유 버튼 내 일부 요소 제거 (#45)

--- src/util/open-share.js
+++ src/util/open-share.js
@@ -53,41 +53,6 @@ export async function openShare({ title, text, url }) {
   const shareOptions = [
     ["Copy", () => copyToClipboard(url)],
     [
-      "WhatsApp",
-      () =>
-        openShareUrl(
-          `https://wa.me/?text=${encodeURIComponent(
-            `${title}. ${text}. Find it here ${url}`
-          )}`
-        ),
-    ],
-    [
-      "Facebook",
-      () =>
-        openShareUrl(`https://www.facebook.com/sharer/sharer.php?u=${safeUrl}`),
-    ],
-    [
-      "Twitter",
-      () =>
-        openShareUrl(
-          `https://twitter.com/intent/tweet?&url=${safeUrl}&text=${safeText}`
-        ),
-    ],
-    [
-      "LinkedIn",
-      () =>
-        openShareUrl(
-          `https://www.linkedin.com/shareArticle?mini=true&url=${safeUrl}&title=${safeTitle}&summary=${safeText}`
-        ),
-    ],
-    [
-      "Reddit",
-      () =>
-        openShareUrl(
-          `https://www.reddit.com/submit?url=${safeUrl}&title=${safeText}`
-        ),
-    ],
-    [
       "Email",
       () =>
         openShareUrl(
@@ -96,53 +61,6 @@ export async function openShare({ title, text, url }) {
           )}`
         ),
     ],
-    [
-      "Hacker News",
-      () =>
-        openShareUrl(
-          `https://news.ycombinator.com/submitlink?u=${safeUrl}&t=${safeText}`
-        ),
-    ],
-    [
-      "Blogger",
-      () =>
-        openShareUrl(
-          `https://www.blogger.com/blog-this.g?n=${safeTitle}&b=${encodeURIComponent(
-            `${text}. Find it <a href="${url}">here</a>.<br/><br/><img width="400" src="${img}" />`
-          )}`
-        ),
-    ],
-    [
-      "Tumblr",
-      () =>
-        openShareUrl(
-          `https://www.tumblr.com/widgets/share/tool/preview?url=${safeUrl}`
-        ),
-    ],
-    [
-      "Pinterest",
-      () =>
-        openShareUrl(
-          `https://www.pinterest.dk/pin/create/button/?url=${safeUrl}&description=${safeText}&media=${safeImg}`
-        ),
-    ],
-    [
-      "ВКонтакте",
-      () =>
-        openShareUrl(
-          `https://vk.com/share.php?url=${safeUrl}&title=${safeTitle}&description=${safeText}&image=${safeImg}`
-        ),
-      "vk",
-    ],
-    [
-      "Одноклассники",
-      () =>
-        openShareUrl(
-          `https://connect.ok.ru/offer?url=${safeUrl}&title=${safeTitle}&imageUrl=${safeImg}`
-        ),
-      "ok",
-    ],
-    ["Mix", () => openShareUrl(`https://mix.com/add?url=${safeUrl}`)],
   ];
 
   // https://www.pinterest.dk/pin/create/button/?url=https://andreasbm.github.io/web-skills&description=A%20visual%20overview%20blah%20blah&media=https://andreasbm.github.io/web-skills/www/og-image.jpg
